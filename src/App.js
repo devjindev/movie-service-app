6b6764f37d8b4fd346c7d05e61ebdc844688a8da
HYJ <hyjinny00@gmail.com>
2021-02-27 22:49:36 +0900

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
   render(){
     const {isLoading} = this.state;
     return (
-      <div>{isLoading? "Loading..." : "We are ready 💜"}</div>
+      <div>
+        {isLoading ? "Loading..." : "We are ready 💜"}
+      </div>
     );
   }
 }
