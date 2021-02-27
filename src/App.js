f8ae470a13bd46f61b077b3949a96bfd92adec69
HYJ <hyjinny00@gmail.com>
2021-02-28 00:10:40 +0900

    HTML 작성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./css/import.css";
 
 class App extends React.Component{
   state = {
@@ -17,21 +18,30 @@ class App extends React.Component{
   render(){
     const {isLoading, movies} = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : movies.map(movie => {
-          console.log(movie);
-          return <Movie 
-            key={movie.id}
-            id={movie.id} 
-            year={movie.year} 
-            title={movie.title} 
-            summary={movie.summary} 
-            poster={movie.medium_cover_image} 
-          />
-        })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loading">
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {
+              movies.map(movie => {
+                return <Movie 
+                  key={movie.id}
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image} 
+                />
+              })
+            }
+          </div>
+        )}
+      </section>
     );
   }
 }
- 
+
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./css/import.css";
 
 function Movie({id, year, title, summary, poster}){
-    return (<h4>{title}</h4>);
+    return (
+        <div class="movies-movie">
+            <img src={poster} alt={title} title={title}></img>
+            <h3>{title}</h3>
+            <h5>{summary}</h5>
+            <p>{summary}</p>
+        </div>
+    );
 }
 
 Movie.propTypes = {
