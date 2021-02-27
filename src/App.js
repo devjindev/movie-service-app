066360366592aa8cf576c96a3b0c34445490a1aa
HYJ <hyjinny00@gmail.com>
2021-02-27 21:52:47 +0900

    js: componentDidMount()

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  
+  state = {
+    isLoading: true
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+  render(){
+    const {isLoading} = this.state;
+    return (
+      <div>{isLoading? "Loading..." : "We are ready 💜"}</div>
+    );
+  }
 }
-
+ 
 export default App;
